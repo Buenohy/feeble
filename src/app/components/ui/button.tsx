b34245d06dcad73f431b2191baf6a6495babaa32
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-03 18:30:44 -0300

    feat: Button ui

---
+++ src/app/components/ui/button.tsx
@@ -0,0 +1,6 @@
+
+export default function Button() {
+  return (
+    <button type="button" className="bg-primary-feeble py-2.75 px-4.5 rounded-full cursor-pointer font-medium text-base">Contact Sales</button>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Button from "./components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background-feeble font-sans dark:bg-background-feeble">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-background-feeble dark:bg-background-feeble sm:items-start">
+    <div className="flex min-h-screen items-center justify-center font-sans bg-linear-to-bl from-primary-feeble/10 to-white/90">
+      <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-linear-to-bl from-primary-feeble/10 to-white/90 sm:items-start">
         <h1 className="text-black">Feeble</h1>
+        <Button />
       </main>
     </div>
   );
